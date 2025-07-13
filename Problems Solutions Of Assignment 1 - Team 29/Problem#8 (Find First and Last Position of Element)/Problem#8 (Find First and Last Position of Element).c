7088189b7f02f6af8d7916b600e62b1a71d6fe99
Mohamed Mosaad <mahmoudmosaad00@gmail.com>
2025-07-13 21:20:27 +0300

    Update Problem#8 (Find First and Last Position of Element).c

--- Problems Solutions Of Assignment 1 - Team 29/Problem#8 (Find First and Last Position of Element)/Problem#8 (Find First and Last Position of Element).c
+++ Problems Solutions Of Assignment 1 - Team 29/Problem#8 (Find First and Last Position of Element)/Problem#8 (Find First and Last Position of Element).c
@@ -1,25 +1,10 @@
 #include <stdio.h>
+
 typedef struct {
     int first;
     int second;
 } Pair;
 
-int lowerBound(int nums[], int numsSize, int target);
-int upperBound(int nums[], int numsSize, int target);
-Pair searchRange(int nums[], int numsSize, int target);
-
-
-int main() {
-    int nums[] = {5, 7, 7, 8, 8, 8, 9};
-    int numsSize = 7;
-    int target = 8;
-    
-    Pair result = searchRange(nums, numsSize, target);
-
-    printf("Output: [%d, %d]\n", result.first, result.second);
-    
-    return 0;
-}
 int lowerBound(int nums[], int numsSize, int target) {
     int left = 0;
     int right = numsSize;
@@ -65,4 +50,16 @@ Pair searchRange(int nums[], int numsSize, int target) {
     result.second = upperBound(nums, numsSize, target);
     
     return result;
+}
+
+int main() {
+    int nums[] = {5, 7, 7, 8, 8, 8, 9};
+    int numsSize = 7;
+    int target = 8;
+    
+    Pair result = searchRange(nums, numsSize, target);
+
+    printf("Output: [%d, %d]\n", result.first, result.second);
+    
+    return 0;
 }
