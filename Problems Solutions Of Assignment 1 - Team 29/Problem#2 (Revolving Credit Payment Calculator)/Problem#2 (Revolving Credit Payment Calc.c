babdc5aee8bb5d56ed313ec6fce50be145e42574
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-07-16 20:53:37 +0300

    Update Problem#2 (Revolving Credit Payment Calc.c

--- Problems Solutions Of Assignment 1 - Team 29/Problem#2 (Revolving Credit Payment Calculator)/Problem#2 (Revolving Credit Payment Calc.c
+++ Problems Solutions Of Assignment 1 - Team 29/Problem#2 (Revolving Credit Payment Calculator)/Problem#2 (Revolving Credit Payment Calc.c
@@ -3,34 +3,49 @@
 #include <math.h>
 #include <stdlib.h>
 
-void PaymentCalculator() {
-    float Balance,totalDue, minPayment, interestRate;
-    printf("Enter your account balance:$ ");
-    scanf("%f", &Balance);
+void PaymentCalculator()
+{
+    float Balance, totalDue, minPayment, interestRate;
     char continueTransaction = 'y';
-    while (continueTransaction == 'y' || continueTransaction == 'Y') {
-        if (Balance < 1000) {
-            interestRate = Balance * 0.015;
-        } else {
-            interestRate = 1000 * 0.015 + (Balance - 1000) * 0.01;
+    while (continueTransaction == 'y' || continueTransaction == 'Y')
+    {
+        printf("Enter your account balance:$ ");
+        scanf("%f", &Balance);
+        if (Balance <= 0)
+        {
+            printf("Balance cannot be Negative!, Try Again\n");
         }
-        totalDue = Balance + interestRate;
-        if (totalDue < 10) {
-            minPayment = totalDue;
-        } else {
-            minPayment = (totalDue * 0.1>10) ? totalDue * 0.1 : 10;
+        else
+        {
+            if (Balance < 1000)
+            {
+                interestRate = Balance * 0.015;
+            }
+            else
+            {
+                interestRate = 1000 * 0.015 + (Balance - 1000) * 0.01;
+                totalDue = Balance + interestRate;
+                if (totalDue < 10)
+                {
+                    minPayment = totalDue;
+                }
+                else
+                {
+                    minPayment = (totalDue * 0.1 > 10) ? totalDue * 0.1 : 10;
+                }
+                printf("Total due: $%.2f\n", totalDue);
+                printf("Minimum payment: $%.2f\n", minPayment);
+                printf("interest rate: $%.2f\n", interestRate);
+                printf("Do you want to continue? (y-Y for yes, n-N for no): ");
+                scanf(" %c", &continueTransaction);
+            }
         }
-        printf("Total due: $%.2f\n", totalDue);
-        printf("Minimum payment: $%.2f\n", minPayment);
-        printf("interest rate: $%.2f\n", interestRate);
-        printf("Do you want to continue? (y-Y for yes, n-N for no): ");
-        scanf(" %c", &continueTransaction);
     }
     printf("Thank you for using our service!\n");
-    printf("Your final balance is: $%.2f\n", Balance);
 }
 
-int main(void) {
+int main(void)
+{
     PaymentCalculator();
     return 0;
 }
